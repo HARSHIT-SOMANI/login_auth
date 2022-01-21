3550e05cb302ba8dfab79db3fdfc578865fea30d
HARSHIT SOMANI <harshitsomani26@gmail.com>
2022-01-21 11:17:43 +0530

    commit6

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='indexd'),
+    path('',views.signup,name='indexd'),
     path('signu',views.signup,name='signupd'),
     path('signi',views.signin,name='signind'),
     path('signo',views.signout,name='signoutd'),
